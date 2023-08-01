45febe62eb0dcb6aaea92ef6a5a0ab64e3c79be7
Andrew-J-Moore <amoore71203@gmail.com>
2023-08-01 10:53:23 -0500

    end of section 7

--- ReStore/client/src/app/layout/Header.tsx
+++ ReStore/client/src/app/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
-import { useStoreContext } from "../context/StoreContext";
+import { useAppSelector } from "../store/configureStore";
 
 const midLinks = [
   { title: "catalog", path: "/catalog" },
@@ -33,11 +33,11 @@ interface Props {
 }
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
-    const {basket} = useStoreContext();
+    const {basket} = useAppSelector(state => state.basket);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
-        <AppBar position="static" sx={{ mb: 4 }}>
+        <AppBar position="sticky" sx={{ mb: 4 }}>
         <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
             <Box display='flex' alignItems='center'>
                 <Typography variant="h6" component={NavLink} 
