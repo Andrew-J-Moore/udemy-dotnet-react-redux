000a26332d47d4f8675873e98716096be5e1a51b
Andrew-J-Moore <amoore71203@gmail.com>
2023-08-04 15:16:28 -0500

    update empty cart behavior on BasketPage

--- ReStore/API/Controllers/BasketController.cs
+++ ReStore/API/Controllers/BasketController.cs
@@ -23,13 +23,6 @@ namespace API.Controllers
 
             if (basket == null) return NotFound();
 
-            if (basket.Items.Count == 0) 
-            {
-                _context.Baskets.Remove(basket);
-                var result = await _context.SaveChangesAsync() > 0;
-                if(result) return NotFound();
-            }
-
             return basket.MapBasketToDto();
         }
 
@@ -64,11 +57,7 @@ namespace API.Controllers
 
             //remove item or reduce quantity
             basket.RemoveItem(productId, quantity);
-            if(basket.Items.Count == 0)
-            {
-                _context.Baskets.Remove(basket);
 
-            }
             //save changes
             var result = await _context.SaveChangesAsync() > 0;
             if(result) return Ok();
